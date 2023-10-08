22c7f082b3019d73b7e29d6bc492439ca42d4dc8
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-08 12:01:37 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *  main - write point
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
