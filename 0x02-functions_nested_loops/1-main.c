00993ef15d9b40dbc910b3e16bdcc35e99d5b605
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-10 08:46:06 -0700

    1

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - chack this code
- * Return: 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	print_alphabet();
-	return (0);
+    print_alphabet();
+    return (0);
 }
