122b2589239c5a7fb307576e07b543e608125419
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-12 15:05:54 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,25 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-	if (x % 3 == 0 && x % 5 != 0)
+	if ((x % 3 == 0) && (x % 5 == 0))
 	{
-	printf(" fizz");
+	printf("FizzBuzz");
 	}
-	else if (x % 5 == 0 && x % 3 != 0)
+	else if (x % 3 == 0)
 	{
-	printf(" Buzz");
+	printf("Fizz");
 	}
-	else if (x % 3 == 0 && x % 5 == 0)
+	else if (x % 5 == 0)
 	{
-	printf(" fizzBuzz");
-	}
-	else if (x == 1)
-	{
-	printf("%d", x);
+	printf("Buzz");
 	}
 	else
 	{
-	printf(" %d", x);
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	printf(" ");
 	}
 	}
 	printf('\n');
