0dbcec388bb0834aa380cd19b16551d3dba10e33
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-19 00:18:18 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,17 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\a')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (src[j] != '\a')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\a';
+	dest[i] = '\0';
 	return (dest);
 }
