7eef18a3cf8392442e9a61eb34b7f709c905a17c
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-19 00:14:21 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concnate two srtings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ * */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\a')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\a')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\a';
+	return (dest);
+}
