a1681a01f4f48eb226a66634c61ad08a9fed5fb4
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-23 08:22:08 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + 1);
+	}
+	return (NULL);
+}
