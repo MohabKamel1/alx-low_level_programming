8f2925e5ffbc4575fe29fa45fa2f796a3d75a1aa
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-27 04:54:09 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Enter data
+ * @argc: input
+ * @argv: input
+ * Return: 0 when erorr else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+			sum = sum * atoi(argv[i]);
+		printf("%d\n", sum);
+	}
+	return (0);
+}
