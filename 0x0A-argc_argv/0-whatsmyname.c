797de09b006e2b6e9f452a2f086663518fa35034
mohabkamel1 <mohabkamel896@gmail.com>
2023-10-27 04:47:46 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Enter data
+ * @argc: input
+ * @argv: input
+ * Return: 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
