80b570ce9d090302060c670254703fda3ed80521
Mohabkamel1 <mohabwael896@gmail.com>
2024-01-11 12:48:45 -0800

    $

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a linked dlistint_t list.
+ * @head: input
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
